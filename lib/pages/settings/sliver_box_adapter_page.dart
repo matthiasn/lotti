4e7f95356048115857e33fc0ad039318ee85c8b8
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-05-20 19:34:59 +0200

    Improve header style on settings pages

--- lib/pages/settings/sliver_box_adapter_page.dart
+++ lib/pages/settings/sliver_box_adapter_page.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:lotti/pages/settings/sliver_box_adapter_page.dart';
+import 'package:lotti/themes/theme.dart';
+import 'package:lotti/widgets/habits/habit_page_app_bar.dart';
 import 'package:lotti/widgets/settings/settings_card.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -65,6 +66,32 @@ class SettingsPage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SliverBoxAdapterPage extends StatelessWidget {
+  const SliverBoxAdapterPage({
+    required this.child,
+    required this.title,
+    super.key,
+  });
+
+  final Widget child;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: styleConfig().negspace,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverTitleBar(title),
+          SliverToBoxAdapter(
+            child: child,
+          )
+        ],
       ),
     );
   }
