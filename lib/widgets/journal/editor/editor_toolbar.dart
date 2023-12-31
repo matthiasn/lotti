35a1c8537f80aa7eedff1cfb4566c7a18702640e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2023-12-31 02:16:15 +0100

    feat: reduce editor toolbar to supported features

--- lib/widgets/journal/editor/editor_toolbar.dart
+++ lib/widgets/journal/editor/editor_toolbar.dart
@@ -1,166 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_quill/flutter_quill.dart';
-import 'package:lotti/blocs/journal/entry_cubit.dart';
-import 'package:lotti/blocs/journal/entry_state.dart';
-import 'package:lotti/get_it.dart';
-import 'package:lotti/services/link_service.dart';
 
 class ToolbarWidget extends StatelessWidget {
-  ToolbarWidget({
+  const ToolbarWidget({
     super.key,
-    this.toolbarIconSize = 20,
-    this.unlinkFn,
   });
-
-  final LinkService linkService = getIt<LinkService>();
-  final double toolbarIconSize;
-  final WrapAlignment toolbarIconAlignment = WrapAlignment.start;
-  final Future<void> Function()? unlinkFn;
 
   @override
   Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    final iconTheme = QuillIconTheme(
-      iconSelectedColor: Theme.of(context).colorScheme.inversePrimary,
-      iconSelectedFillColor: Theme.of(context).primaryColor,
-      iconUnselectedColor: Theme.of(context).colorScheme.outline,
-    );
-
-    return BlocBuilder<EntryCubit, EntryState>(
-      builder: (
-        context,
-        EntryState snapshot,
-      ) {
-        final cubit = context.read<EntryCubit>();
-        final controller = cubit.controller;
-        final id = context.read<EntryCubit>().entryId;
-
-        return Material(
-          elevation: 1,
-          child: QuillToolbar(
-            key: key,
-            configurations: QuillToolbarConfigurations(
-              toolbarSize: 44,
-              toolbarSectionSpacing: 0,
-              toolbarIconAlignment: toolbarIconAlignment,
-              multiRowsDisplay: false,
-            ),
-            // children: [
-            //   ToggleStyleButton(
-            //     attribute: Attribute.bold,
-            //     icon: Icons.format_bold,
-            //     iconSize: toolbarIconSize,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.italic,
-            //     icon: Icons.format_italic,
-            //     iconSize: toolbarIconSize,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.underline,
-            //     icon: Icons.format_underline,
-            //     iconSize: toolbarIconSize,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.strikeThrough,
-            //     icon: Icons.format_strikethrough,
-            //     iconSize: toolbarIconSize,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.inlineCode,
-            //     icon: Icons.code,
-            //     iconSize: toolbarIconSize,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   SelectHeaderStyleButton(
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconSize: toolbarIconSize,
-            //     iconTheme: iconTheme,
-            //     attributes: const [
-            //       Attribute.h1,
-            //       Attribute.h2,
-            //     ],
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.ul,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     icon: Icons.format_list_bulleted,
-            //     iconSize: toolbarIconSize,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.ol,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     icon: Icons.format_list_numbered,
-            //     iconSize: toolbarIconSize,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ToggleStyleButton(
-            //     attribute: Attribute.codeBlock,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     icon: Icons.code,
-            //     iconSize: toolbarIconSize,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   ClearFormatButton(
-            //     icon: Icons.format_clear,
-            //     iconSize: toolbarIconSize,
-            //     controller: controller,
-            //     afterButtonPressed: cubit.focus,
-            //     iconTheme: iconTheme,
-            //   ),
-            //   IconButton(
-            //     icon: Icon(
-            //       Icons.add_link,
-            //       color: Theme.of(context).colorScheme.outline,
-            //     ),
-            //     iconSize: toolbarIconSize,
-            //     tooltip: localizations.journalLinkFromHint,
-            //     onPressed: () => linkService.linkFrom(id),
-            //   ),
-            //   IconButton(
-            //     icon: Icon(
-            //       MdiIcons.target,
-            //       color: Theme.of(context).colorScheme.outline,
-            //     ),
-            //     iconSize: toolbarIconSize,
-            //     tooltip: localizations.journalLinkToHint,
-            //     onPressed: () => linkService.linkTo(id),
-            //   ),
-            //   if (unlinkFn != null)
-            //     IconButton(
-            //       icon: Icon(
-            //         MdiIcons.closeCircleOutline,
-            //         color: Theme.of(context).colorScheme.outline,
-            //       ),
-            //       iconSize: toolbarIconSize,
-            //       tooltip: localizations.journalUnlinkHint,
-            //       onPressed: unlinkFn,
-            //     ),
-            // ],
-          ),
-        );
-      },
+    return Material(
+      elevation: 1,
+      child: QuillToolbar(
+        key: key,
+        configurations: const QuillToolbarConfigurations(
+          toolbarSize: 44,
+          toolbarSectionSpacing: 0,
+          toolbarIconAlignment: WrapAlignment.start,
+          multiRowsDisplay: false,
+          showColorButton: false,
+          showFontFamily: false,
+          showBackgroundColorButton: false,
+          showSubscript: false,
+          showSuperscript: false,
+          showIndent: false,
+          showFontSize: false,
+        ),
+      ),
     );
   }
 }
--- lib/widgets/journal/entry_details/entry_detail_header.dart
+++ lib/widgets/journal/entry_details/entry_detail_header.dart
@@ -5,23 +5,29 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lotti/blocs/journal/entry_cubit.dart';
 import 'package:lotti/blocs/journal/entry_state.dart';
 import 'package:lotti/classes/journal_entities.dart';
+import 'package:lotti/get_it.dart';
+import 'package:lotti/services/link_service.dart';
 import 'package:lotti/themes/colors.dart';
 import 'package:lotti/widgets/journal/entry_details/delete_icon_widget.dart';
 import 'package:lotti/widgets/journal/entry_details/save_button.dart';
 import 'package:lotti/widgets/journal/entry_details/share_button_widget.dart';
 import 'package:lotti/widgets/journal/tags/tag_add.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class EntryDetailHeader extends StatelessWidget {
   const EntryDetailHeader({
     this.inLinkedEntries = false,
     super.key,
+    this.unlinkFn,
   });
 
   final bool inLinkedEntries;
+  final Future<void> Function()? unlinkFn;
 
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
+    final linkService = getIt<LinkService>();
 
     return BlocBuilder<EntryCubit, EntryState>(
       builder: (context, EntryState state) {
@@ -31,6 +37,8 @@ class EntryDetailHeader extends StatelessWidget {
         if (item == null) {
           return const SizedBox.shrink();
         }
+
+        final id = item.meta.id;
 
         return Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -75,6 +83,23 @@ class EntryDetailHeader extends StatelessWidget {
                 DeleteIconWidget(beamBack: !inLinkedEntries),
                 const ShareButtonWidget(),
                 TagAddIconWidget(),
+                const SizedBox(width: 20),
+                IconButton(
+                  icon: const Icon(Icons.add_link),
+                  tooltip: localizations.journalLinkFromHint,
+                  onPressed: () => linkService.linkFrom(id),
+                ),
+                IconButton(
+                  icon: Icon(MdiIcons.target),
+                  tooltip: localizations.journalLinkToHint,
+                  onPressed: () => linkService.linkTo(id),
+                ),
+                if (unlinkFn != null)
+                  IconButton(
+                    icon: Icon(MdiIcons.closeCircleOutline),
+                    tooltip: localizations.journalUnlinkHint,
+                    onPressed: unlinkFn,
+                  ),
               ],
             ),
             const SaveButton(),
