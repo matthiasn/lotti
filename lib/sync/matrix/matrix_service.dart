7ce4174a5c98aeb699aad1b3a1eefdfc81e010db
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-02-03 20:49:54 +0100

    refactor: matrix service

--- lib/sync/matrix/matrix_service.dart
+++ lib/sync/matrix/matrix_service.dart
@@ -24,15 +24,79 @@ import 'package:matrix/matrix.dart';
 import 'package:path_provider/path_provider.dart';
 
 class MatrixService {
-  final client = Client(
-    'lotti',
-    databaseBuilder: (_) async {
-      final dir = await getApplicationSupportDirectory();
-      final db = HiveCollectionsDatabase('matrix__chat', dir.path);
-      await db.open();
-      return db;
-    },
-  );
+  MatrixService()
+      : client = Client(
+          'lotti',
+          databaseBuilder: (_) async {
+            final dir = await getApplicationDocumentsDirectory();
+            final db = HiveCollectionsDatabase('lotti_sync', dir.path);
+            await db.open();
+            return db;
+          },
+        ) {
+    login().then((value) => printUnverified()).then((value) => listen());
+
+    Timer.periodic(const Duration(seconds: 15), (_) {
+      sendMatrixMsg(
+        'PING ${DateTime.now()} ${Platform.localHostname} ${Platform.isIOS}',
+      );
+    });
+  }
+
+  Future<void> login() async {
+    const homeServer = String.fromEnvironment('MATRIX_HOME_SERVER');
+    const userName = String.fromEnvironment('MATRIX_USER');
+    const password = String.fromEnvironment('MATRIX_PASSWORD');
+    const roomId = String.fromEnvironment('MATRIX_ROOM_ID');
+
+    await client.checkHomeserver(
+      Uri.parse(homeServer),
+    );
+
+    await client.init(
+      waitForFirstSync: false,
+      waitUntilLoadCompletedLoaded: false,
+    );
+
+    // TODO(unassigned): find non-deprecated solution
+    // ignore: deprecated_member_use
+    if (client.loginState == LoginState.loggedOut) {
+      final loginResponse = await client.login(
+        LoginType.mLoginPassword,
+        identifier: AuthenticationUserIdentifier(user: userName),
+        password: password,
+      );
+
+      debugPrint('MatrixService userId ${loginResponse.userId}');
+      debugPrint(
+        'MatrixService loginResponse deviceId ${loginResponse.deviceId}',
+      );
+    }
+
+    final joinRes = await client.joinRoom(roomId).onError((
+      error,
+      stackTrace,
+    ) {
+      debugPrint('MatrixService join error $error');
+      return error.toString();
+    });
+
+    debugPrint('MatrixService joinRes $joinRes');
+  }
+
+  Future<void> loadArchive() async {
+    final rooms = await client.loadArchive();
+    debugPrint('Matrix $rooms');
+  }
+
+  Future<void> printUnverified() async {
+    final unverified = client.unverifiedDevices;
+    final keyVerification = await unverified.firstOrNull?.startVerification();
+    debugPrint('Matrix keyVerification ${keyVerification?.qrCode}');
+    debugPrint('Matrix unverified ${unverified.length} $unverified');
+  }
+
+  final Client client;
 
   final ConnectivityService _connectivityService = getIt<ConnectivityService>();
   final FgBgService _fgBgService = getIt<FgBgService>();
@@ -45,19 +109,16 @@ class MatrixService {
     fgBgSubscription.cancel();
   }
 
-  Future<void> sendMatrixMsg(String msg) async {
+  Future<void> listen() async {
     try {
       client.onLoginStateChanged.stream.listen((LoginState loginState) {
         debugPrint('LoginState: $loginState');
       });
 
       client.onEvent.stream.listen((EventUpdate eventUpdate) {
-        debugPrint('New event update! $eventUpdate');
+        //debugPrint('New event update! $eventUpdate');
       });
 
-      const homeServer = String.fromEnvironment('MATRIX_HOME_SERVER');
-      const userName = String.fromEnvironment('MATRIX_USER');
-      const password = String.fromEnvironment('MATRIX_PASSWORD');
       const roomId = String.fromEnvironment('MATRIX_ROOM_ID');
 
       final room = client.getRoomById(roomId);
@@ -68,11 +129,11 @@ class MatrixService {
           'MatrixService onRoomState.stream.listen plaintextBody: ${eventUpdate.plaintextBody}',
         );
 
-        final t = await room?.getTimeline();
-        await t?.setReadMarker(
-          eventId: eventUpdate.eventId,
-          public: true,
-        );
+        // final t = await room?.getTimeline();
+        // await t?.setReadMarker(
+        //   eventId: eventUpdate.eventId,
+        //   public: true,
+        // );
 
         final attachmentMimetype = eventUpdate.attachmentMimetype;
         if (attachmentMimetype.isNotEmpty) {
@@ -80,59 +141,27 @@ class MatrixService {
         }
       });
 
-      await client.checkHomeserver(
-        Uri.parse(homeServer),
-      );
-
-      // TODO(unassigned): find non-deprecated solution
-      // ignore: deprecated_member_use
-      if (client.loginState == LoginState.loggedOut) {
-        final loginResponse = await client.login(
-          LoginType.mLoginPassword,
-          identifier: AuthenticationUserIdentifier(user: userName),
-          password: password,
-        );
-
-        debugPrint('MatrixService userId ${loginResponse.userId}');
-        debugPrint(
-          'MatrixService loginResponse deviceId ${loginResponse.deviceId}',
-        );
-      }
-
-      final unverified = client.unverifiedDevices;
-      final keyVerification = await unverified.firstOrNull?.startVerification();
-
-      debugPrint('Matrix keyVerification ${keyVerification?.qrCode}');
-      debugPrint('Matrix unverified ${unverified.length} $unverified');
-
-      final timeline = await room?.getTimeline(
-        onInsert: (i) async {
-          debugPrint('New message in timeline! $i');
-        },
-      );
-
-      final joinRes = await client.joinRoom(roomId).onError((
-        error,
-        stackTrace,
-      ) {
-        debugPrint('MatrixService join error $error');
-        return error.toString();
-      });
-
-      debugPrint('MatrixService joinRes $joinRes');
-
-      Timer.periodic(const Duration(seconds: 15), (_) {
-        room?.sendTextEvent(
-          '${DateTime.now()} ${Platform.localHostname} ${Platform.isIOS}',
-        );
-      });
-
-      await room?.sendTextEvent(msg);
-
-      final rooms = await client.loadArchive();
-      debugPrint('Matrix $rooms');
+      // await client.checkHomeserver(
+      //   Uri.parse(homeServer),
+      // );
+      //
+      // final timeline = await room?.getTimeline(
+      //   onInsert: (i) async {
+      //     debugPrint('New message in timeline! $i');
+      //   },
+      // );
     } catch (e) {
       debugPrint('$e');
+    }
+  }
+
+  Future<void> sendMatrixMsg(String msg) async {
+    try {
+      const roomId = String.fromEnvironment('MATRIX_ROOM_ID');
+      final room = client.getRoomById(roomId);
+      await room?.sendTextEvent(msg);
+    } catch (e) {
+      debugPrint('MATRIX: Error sending message: $e');
     }
   }
 
