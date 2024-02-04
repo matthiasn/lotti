9e9f2c462b5ca2c88cdeca4784347c5f94e154cf
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-02-04 20:08:45 +0100

    feat: hide matrix sync behind feature flag

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -4,3 +4,4 @@ const allowInvalidCertFlag = 'allow_invalid_cert';
 const enableSyncFlag = 'enable_sync';
 const recordLocationFlag = 'record_location';
 const autoTranscribeFlag = 'auto_transcribe';
+const enableMatrixFlag = 'enable_matrix';
