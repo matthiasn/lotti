16c1d91b4f1ae631298aee2f8e7fbc8b954e048b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-02-15 19:15:30 +0100

    feat: maintenance task for transcribing audio without transcript

--- lib/pages/settings/speech/speech_settings_page.dart
+++ lib/pages/settings/speech/speech_settings_page.dart
@@ -3,7 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lotti/blocs/settings/speech/speech_settings_cubit.dart';
 import 'package:lotti/blocs/settings/speech/speech_settings_state.dart';
+import 'package:lotti/database/maintenance.dart';
+import 'package:lotti/get_it.dart';
 import 'package:lotti/pages/settings/sliver_box_adapter_page.dart';
+import 'package:lotti/widgets/settings/settings_card.dart';
 import 'package:lotti/widgets/settings/speech/whisper_model_card.dart';
 
 class SpeechSettingsPage extends StatelessWidget {
@@ -23,6 +26,13 @@ class SpeechSettingsPage extends StatelessWidget {
             child: Column(
               children: [
                 ...snapshot.availableModels.map(WhisperModelCard.new),
+                const SizedBox(height: 30),
+                SettingsCard(
+                  title:
+                      localizations.maintenanceTranscribeAudioWithoutTranscript,
+                  onTap: () =>
+                      getIt<Maintenance>().transcribeAudioWithoutTranscript(),
+                ),
               ],
             ),
           );
