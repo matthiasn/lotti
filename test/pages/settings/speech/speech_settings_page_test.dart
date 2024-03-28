13b3d3844f3e97b1cbf4d05b9a8d56de4dc20805
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-03-29 00:04:50 +0100

    feat: hide whisper model download for now

--- test/pages/settings/speech/speech_settings_page_test.dart
+++ test/pages/settings/speech/speech_settings_page_test.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lotti/database/logging_db.dart';
 import 'package:lotti/database/settings_db.dart';
 import 'package:lotti/get_it.dart';
-import 'package:lotti/pages/settings/speech/speech_settings_page.dart';
 import 'package:lotti/services/asr_service.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:path/path.dart' as p;
@@ -13,7 +11,6 @@ import 'package:path_provider/path_provider.dart';
 
 import '../../../helpers/path_provider.dart';
 import '../../../mocks/mocks.dart';
-import '../../../widget_test_utils.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -46,59 +43,59 @@ void main() {
 
     tearDown(getIt.reset);
 
-    testWidgets('Available models are displayed', (tester) async {
-      await tester.pumpWidget(
-        makeTestableWidget(
-          ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxHeight: 1000,
-              maxWidth: 1000,
-            ),
-            child: const SpeechSettingsPage(),
-          ),
-        ),
-      );
-
-      await tester.pumpAndSettle();
-
-      expect(find.text('tiny'), findsOneWidget);
-      expect(find.text('tiny.en'), findsOneWidget);
-
-      final tinyDownloadButton = find.bySemanticsLabel('download tiny');
-      expect(tinyDownloadButton, findsOneWidget);
-      await tester.tap(tinyDownloadButton);
-      await tester.pumpAndSettle();
-
-      final tinyEnDownloadButton = find.bySemanticsLabel('download tiny.en');
-      expect(tinyEnDownloadButton, findsOneWidget);
-      await tester.tap(tinyEnDownloadButton);
-      await tester.pumpAndSettle();
-
-      final baseDownloadButton = find.bySemanticsLabel('download base');
-      expect(baseDownloadButton, findsOneWidget);
-      await tester.tap(baseDownloadButton);
-      await tester.pumpAndSettle();
-
-      final baseEnDownloadButton = find.bySemanticsLabel('download base.en');
-      expect(baseEnDownloadButton, findsOneWidget);
-      await tester.tap(baseEnDownloadButton);
-      await tester.pumpAndSettle();
-
-      final smallDownloadButton = find.bySemanticsLabel('download small');
-      expect(smallDownloadButton, findsOneWidget);
-      await tester.tap(smallDownloadButton);
-      await tester.pumpAndSettle();
-
-      final smallEnDownloadButton = find.bySemanticsLabel('download small.en');
-      expect(smallEnDownloadButton, findsOneWidget);
-      await tester.tap(smallEnDownloadButton);
-      await tester.pumpAndSettle();
-
-      final mediumEnDownloadButton =
-          find.bySemanticsLabel('download medium.en');
-      expect(mediumEnDownloadButton, findsOneWidget);
-      await tester.tap(mediumEnDownloadButton);
-      await tester.pumpAndSettle();
-    });
+    // testWidgets('Available models are displayed', (tester) async {
+    //   await tester.pumpWidget(
+    //     makeTestableWidget(
+    //       ConstrainedBox(
+    //         constraints: const BoxConstraints(
+    //           maxHeight: 1000,
+    //           maxWidth: 1000,
+    //         ),
+    //         child: const SpeechSettingsPage(),
+    //       ),
+    //     ),
+    //   );
+    //
+    //   await tester.pumpAndSettle();
+    //
+    //   expect(find.text('tiny'), findsOneWidget);
+    //   expect(find.text('tiny.en'), findsOneWidget);
+    //
+    //   final tinyDownloadButton = find.bySemanticsLabel('download tiny');
+    //   expect(tinyDownloadButton, findsOneWidget);
+    //   await tester.tap(tinyDownloadButton);
+    //   await tester.pumpAndSettle();
+    //
+    //   final tinyEnDownloadButton = find.bySemanticsLabel('download tiny.en');
+    //   expect(tinyEnDownloadButton, findsOneWidget);
+    //   await tester.tap(tinyEnDownloadButton);
+    //   await tester.pumpAndSettle();
+    //
+    //   final baseDownloadButton = find.bySemanticsLabel('download base');
+    //   expect(baseDownloadButton, findsOneWidget);
+    //   await tester.tap(baseDownloadButton);
+    //   await tester.pumpAndSettle();
+    //
+    //   final baseEnDownloadButton = find.bySemanticsLabel('download base.en');
+    //   expect(baseEnDownloadButton, findsOneWidget);
+    //   await tester.tap(baseEnDownloadButton);
+    //   await tester.pumpAndSettle();
+    //
+    //   final smallDownloadButton = find.bySemanticsLabel('download small');
+    //   expect(smallDownloadButton, findsOneWidget);
+    //   await tester.tap(smallDownloadButton);
+    //   await tester.pumpAndSettle();
+    //
+    //   final smallEnDownloadButton = find.bySemanticsLabel('download small.en');
+    //   expect(smallEnDownloadButton, findsOneWidget);
+    //   await tester.tap(smallEnDownloadButton);
+    //   await tester.pumpAndSettle();
+    //
+    //   final mediumEnDownloadButton =
+    //       find.bySemanticsLabel('download medium.en');
+    //   expect(mediumEnDownloadButton, findsOneWidget);
+    //   await tester.tap(mediumEnDownloadButton);
+    //   await tester.pumpAndSettle();
+    // });
   });
 }
