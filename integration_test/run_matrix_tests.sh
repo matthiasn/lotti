2aee48fe4f8757fcc8b6ced5c0990d9ff368509e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-04-04 23:45:25 +0200

    test: use individual users for Matrix clients

--- integration_test/run_matrix_tests.sh
+++ integration_test/run_matrix_tests.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # Create user name
-uuid=$(uuidgen)
-TEST_USER="$(tr '[:upper:]' '[:lower:]' <<< "$uuid")"
+uuid1=$(uuidgen)
+uuid2=$(uuidgen)
+TEST_USER1="$(tr '[:upper:]' '[:lower:]' <<< "$uuid1")"
+TEST_USER2="$(tr '[:upper:]' '[:lower:]' <<< "$uuid2")"
 
 # Go to dendrite docker directory that contains config and keys
 cd ../dendrite/build/docker/config || exit
 
 # Create user using the create-account binary in dendrite build
-../../../bin/create-account -config dendrite.yaml -username "$TEST_USER" -admin -password "?Secret123@!"
+../../../bin/create-account -config dendrite.yaml -username "$TEST_USER1" -admin -password "?Secret123@!"
+../../../bin/create-account -config dendrite.yaml -username "$TEST_USER2" -admin -password "?Secret123@!"
 cd - > /dev/null || exit
 
-fvm flutter test integration_test/matrix_service_test.dart --dart-define=TEST_USER="@$TEST_USER:localhost"
+fvm flutter test integration_test/matrix_service_test.dart \
+    --dart-define=TEST_USER1="@$TEST_USER1:localhost" \
+    --dart-define=TEST_USER2="@$TEST_USER2:localhost"
