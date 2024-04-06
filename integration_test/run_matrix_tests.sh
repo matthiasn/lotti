6d3c7dbaa73ffeed8494eb6b4131ebb7f30a79c1
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-04-06 20:15:51 +0200

    test: remove dependency on local dendrite & toxiproxy in test

--- integration_test/run_matrix_tests.sh
+++ integration_test/run_matrix_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Go to directory of script
+cd "$(dirname "$0")" || exit
 
 # Create user name
 uuid1=$(uuidgen)
@@ -6,16 +8,15 @@ uuid2=$(uuidgen)
 TEST_USER1="$(tr '[:upper:]' '[:lower:]' <<< "$uuid1")"
 TEST_USER2="$(tr '[:upper:]' '[:lower:]' <<< "$uuid2")"
 
-# Go to dendrite docker directory that contains config and keys
-cd ../dendrite/build/docker/config || exit
-
-# Create user using the create-account binary in dendrite build
-../../../bin/create-account -config dendrite.yaml -username "$TEST_USER1" -admin -password "?Secret123@!"
-../../../bin/create-account -config dendrite.yaml -username "$TEST_USER2" -admin -password "?Secret123@!"
+cd docker || exit
+docker compose exec dendrite create-account -config dendrite.yaml -username "$TEST_USER1" -admin -password "?Secret123@"
+docker compose exec dendrite create-account -config dendrite.yaml -username "$TEST_USER2" -admin -password "?Secret123@"
 cd - > /dev/null || exit
+cd ..
 
 fvm flutter test integration_test/matrix_service_test.dart \
 --dart-define=TEST_USER1="@$TEST_USER1:localhost" \
 --dart-define=TEST_USER2="@$TEST_USER2:localhost" \
 --dart-define=SLOW_NETWORK="$SLOW_NETWORK"
 
+cd - || exit
