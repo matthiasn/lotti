09fb35293d08065bc4a268edf165663d813cd443
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-04-06 22:48:28 +0200

    ci: run Matrix test with simulated bad network

--- integration_test/setup_toxiproxy_docker.sh
+++ integration_test/setup_toxiproxy_docker.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")" || exit
 
 cd docker || exit
 docker compose exec toxiproxy /toxiproxy-cli create -l toxiproxy:18008 -u dendrite:8008 dendrite-proxy
-docker compose exec toxiproxy /toxiproxy-cli toxic add -t latency -a latency=1000 dendrite-proxy
+docker compose exec toxiproxy /toxiproxy-cli toxic add -t latency -a latency=500 dendrite-proxy
 docker compose exec toxiproxy /toxiproxy-cli toxic add -t bandwidth -a rate=100 dendrite-proxy
 cd ..
