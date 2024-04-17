844cb481719cd1471accde35d9ca36032ca6c8e0
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-04-17 22:46:18 +0200

    feat: create and leave room

--- lib/sync/matrix/room.dart
+++ lib/sync/matrix/room.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import 'package:lotti/database/logging_db.dart';
+import 'package:lotti/database/settings_db.dart';
 import 'package:lotti/get_it.dart';
+import 'package:lotti/sync/matrix/consts.dart';
 import 'package:lotti/sync/matrix/matrix_service.dart';
 import 'package:matrix/matrix.dart';
 
@@ -59,8 +61,34 @@ Future<String> createMatrixRoom({
   );
   final room = client.getRoomById(roomId);
   await room?.enableEncryption();
+
+  await saveMatrixRoom(client: client, roomId: roomId);
   return roomId;
 }
+
+Future<void> leaveMatrixRoom({
+  required Client client,
+}) async {
+  final roomId = await getMatrixRoom(client: client);
+
+  if (roomId != null) {
+    await client.leaveRoom(roomId);
+    await getIt<SettingsDb>().removeSettingsItem(matrixRoomKey);
+  }
+}
+
+Future<void> saveMatrixRoom({
+  required Client client,
+  required String roomId,
+}) async {
+  await getIt<SettingsDb>().saveSettingsItem(
+    matrixRoomKey,
+    roomId,
+  );
+}
+
+Future<String?> getMatrixRoom({required Client client}) =>
+    getIt<SettingsDb>().itemByKey(matrixRoomKey);
 
 Future<void> inviteToMatrixRoom({
   required MatrixService service,
---
+++ lib/sync/state/matrix_room_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'matrix_room_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$matrixRoomControllerHash() =>
+    r'6d3d0ff9f4a356b32b19fb5aabc1368c8dd47fbe';
+
+/// See also [MatrixRoomController].
+@ProviderFor(MatrixRoomController)
+final matrixRoomControllerProvider =
+    AutoDisposeAsyncNotifierProvider<MatrixRoomController, String?>.internal(
+  MatrixRoomController.new,
+  name: r'matrixRoomControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$matrixRoomControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MatrixRoomController = AutoDisposeAsyncNotifier<String?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
