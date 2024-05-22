a87c995c702ad25d8a2fe3b4717ce34e69aecafc
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-05-22 23:29:42 +0200

    refactor: simplify query

--- test/pages/dashboards/dashboard_page_test.dart
+++ test/pages/dashboards/dashboard_page_test.dart
@@ -61,9 +61,7 @@ void main() {
       when(
         () => mockJournalDb.watchDashboardById(testDashboardConfig.id),
       ).thenAnswer(
-        (_) => Stream<List<DashboardDefinition>>.fromIterable([
-          <DashboardDefinition>[testDashboardConfig],
-        ]),
+        (_) => Stream<DashboardDefinition>.fromIterable([testDashboardConfig]),
       );
 
       when(
