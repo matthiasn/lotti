d596a0b689cc4d09c779adc42686e2c1433137bf
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-06-01 23:40:05 +0200

    refactor: move sending message to extension

--- lib/sync/matrix/send_message.dart
+++ lib/sync/matrix/send_message.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -16,157 +17,161 @@ import 'package:lotti/utils/file_utils.dart';
 import 'package:lotti/utils/image_utils.dart';
 import 'package:matrix/matrix.dart';
 
-/// Sends a Matrix message for cross-device state synchronization. Takes a
-/// [SyncMessage] and also requires the system's [MatrixService]. A room can
-/// optionally be specified, e.g. in testing. Otherwise, the service's `syncRoom`
-/// is used.
-/// Also updates some stats on sent message counts on the [service].
-/// The send function will terminate early (and thus refuse to send anything)
-/// when there are users with unverified device in the room.
-Future<bool> sendMessage(
-  SyncMessage syncMessage, {
-  required MatrixService service,
-  required String? myRoomId,
-}) async {
-  final loggingDb = getIt<LoggingDb>();
-
-  try {
-    final msg = json.encode(syncMessage);
-    final syncRoom = service.syncRoom;
-    final roomId = myRoomId ?? service.syncRoomId;
-
-    void incrementSentCount() {
-      service.sentCount = service.sentCount + 1;
-      service.messageCountsController.add(
-        MatrixStats(
-          messageCounts: service.messageCounts,
-          sentCount: service.sentCount,
-        ),
-      );
-    }
-
-    if (service.getUnverifiedDevices().isNotEmpty) {
-      loggingDb.captureException(
-        'Unverified devices found',
-        domain: 'MATRIX_SERVICE',
-        subDomain: 'sendMatrixMsg',
-      );
-      return false;
-    }
-
-    if (roomId == null) {
-      loggingDb.captureEvent(
-        configNotFound,
-        domain: 'MATRIX_SERVICE',
-        subDomain: 'sendMatrixMsg',
-      );
-      return false;
-    }
-
-    loggingDb.captureEvent(
-      'trying to send text message to $syncRoom',
-      domain: 'MATRIX_SERVICE',
-      subDomain: 'sendMatrixMsg',
-    );
-
-    final eventId = await syncRoom?.sendTextEvent(
-      base64.encode(utf8.encode(msg)),
-      msgtype: syncMessageType,
-      parseCommands: false,
-      parseMarkdown: false,
-    );
-
-    incrementSentCount();
-
-    loggingDb.captureEvent(
-      'sent text message to $syncRoom with event ID $eventId',
-      domain: 'MATRIX_SERVICE',
-      subDomain: 'sendMatrixMsg',
-    );
-
-    final docDir = getDocumentsDirectory();
-
-    if (syncMessage is SyncJournalEntity) {
-      final journalEntity = syncMessage.journalEntity;
-
-      final shouldResendAttachments =
-          await getIt<JournalDb>().getConfigFlag(resendAttachments);
-
-      await journalEntity.maybeMap(
-        journalAudio: (JournalAudio journalAudio) async {
-          if (shouldResendAttachments ||
-              syncMessage.status == SyncEntryStatus.initial) {
-            final relativePath = AudioUtils.getRelativeAudioPath(journalAudio);
-            final fullPath = AudioUtils.getAudioPath(journalAudio, docDir);
-            final bytes = await File(fullPath).readAsBytes();
-
-            loggingDb.captureEvent(
-              'trying to send $relativePath file message to $syncRoom',
-              domain: 'MATRIX_SERVICE',
-              subDomain: 'sendMatrixMsg',
-            );
-            final eventId = await syncRoom?.sendFileEvent(
-              MatrixFile(
-                bytes: bytes,
-                name: fullPath,
-              ),
-              extraContent: {
-                'relativePath': relativePath,
-              },
-            );
-            incrementSentCount();
-
-            loggingDb.captureEvent(
-              'sent $relativePath file message to $syncRoom, event ID $eventId',
-              domain: 'MATRIX_SERVICE',
-              subDomain: 'sendMatrixMsg',
-            );
-          }
-        },
-        journalImage: (JournalImage journalImage) async {
-          if (shouldResendAttachments ||
-              syncMessage.status == SyncEntryStatus.initial) {
-            final relativePath = getRelativeImagePath(journalImage);
-            final fullPath = getFullImagePath(journalImage);
-            final bytes = await File(fullPath).readAsBytes();
-
-            loggingDb.captureEvent(
-              'trying to send $relativePath file message to $syncRoom',
-              domain: 'MATRIX_SERVICE',
-              subDomain: 'sendMatrixMsg',
-            );
-
-            final eventId = await syncRoom?.sendFileEvent(
-              MatrixFile(
-                bytes: bytes,
-                name: fullPath,
-              ),
-              extraContent: {
-                'relativePath': relativePath,
-              },
-            );
-
-            incrementSentCount();
-
-            loggingDb.captureEvent(
-              'sent $relativePath file message to $syncRoom, event ID $eventId',
-              domain: 'MATRIX_SERVICE',
-              subDomain: 'sendMatrixMsg',
-            );
-          }
-        },
-        orElse: () {},
-      );
-    }
-    return true;
-  } catch (e, stackTrace) {
-    debugPrint('MATRIX: Error sending message: $e');
-    loggingDb.captureException(
-      e,
-      domain: 'MATRIX_SERVICE',
-      subDomain: 'sendMatrixMsg',
-      stackTrace: stackTrace,
+extension SendExtension on MatrixService {
+  void incrementSentCount() {
+    sentCount = sentCount + 1;
+    messageCountsController.add(
+      MatrixStats(
+        messageCounts: messageCounts,
+        sentCount: sentCount,
+      ),
     );
   }
-  return false;
+
+  /// Sends a Matrix message for cross-device state synchronization. Takes a
+  /// [SyncMessage] and also requires the system's [MatrixService]. A room can
+  /// optionally be specified, e.g. in testing. Otherwise, the service's `syncRoom`
+  /// is used.
+  /// Also updates some stats on sent message counts on the [MatrixService].
+  /// The send function will terminate early (and thus refuse to send anything)
+  /// when there are users with unverified device in the room.
+  Future<bool> sendMessage(
+    SyncMessage syncMessage, {
+    required String? myRoomId,
+  }) async {
+    final loggingDb = getIt<LoggingDb>();
+
+    try {
+      final msg = json.encode(syncMessage);
+      final roomId = myRoomId ?? syncRoomId;
+
+      if (getUnverifiedDevices().isNotEmpty) {
+        loggingDb.captureException(
+          'Unverified devices found',
+          domain: 'MATRIX_SERVICE',
+          subDomain: 'sendMatrixMsg',
+        );
+        return false;
+      }
+
+      if (roomId == null) {
+        loggingDb.captureEvent(
+          configNotFound,
+          domain: 'MATRIX_SERVICE',
+          subDomain: 'sendMatrixMsg',
+        );
+        return false;
+      }
+
+      loggingDb.captureEvent(
+        'trying to send text message to $syncRoom',
+        domain: 'MATRIX_SERVICE',
+        subDomain: 'sendMatrixMsg',
+      );
+
+      final eventId = await syncRoom?.sendTextEvent(
+        base64.encode(utf8.encode(msg)),
+        msgtype: syncMessageType,
+        parseCommands: false,
+        parseMarkdown: false,
+      );
+
+      incrementSentCount();
+
+      loggingDb.captureEvent(
+        'sent text message to $syncRoom with event ID $eventId',
+        domain: 'MATRIX_SERVICE',
+        subDomain: 'sendMatrixMsg',
+      );
+
+      final docDir = getDocumentsDirectory();
+
+      if (syncMessage is SyncJournalEntity) {
+        final journalEntity = syncMessage.journalEntity;
+
+        final shouldResendAttachments =
+            await getIt<JournalDb>().getConfigFlag(resendAttachments);
+
+        await journalEntity.maybeMap(
+          journalAudio: (JournalAudio journalAudio) async {
+            if (shouldResendAttachments ||
+                syncMessage.status == SyncEntryStatus.initial) {
+              unawaited(
+                sendFile(
+                  fullPath: AudioUtils.getAudioPath(
+                    journalAudio,
+                    docDir,
+                  ),
+                  relativePath: AudioUtils.getRelativeAudioPath(journalAudio),
+                ),
+              );
+            }
+          },
+          journalImage: (JournalImage journalImage) async {
+            if (shouldResendAttachments ||
+                syncMessage.status == SyncEntryStatus.initial) {
+              unawaited(
+                sendFile(
+                  fullPath: getFullImagePath(journalImage),
+                  relativePath: getRelativeImagePath(journalImage),
+                ),
+              );
+            }
+          },
+          orElse: () {},
+        );
+      }
+      return true;
+    } catch (e, stackTrace) {
+      debugPrint('MATRIX: Error sending message: $e');
+      loggingDb.captureException(
+        e,
+        domain: 'MATRIX_SERVICE',
+        subDomain: 'sendMatrixMsg',
+        stackTrace: stackTrace,
+      );
+    }
+    return false;
+  }
+
+  /// Sends a file attachment to the sync room.
+  Future<void> sendFile({
+    required String fullPath,
+    required String relativePath,
+  }) async {
+    final loggingDb = getIt<LoggingDb>()
+      ..captureEvent(
+        'trying to send $relativePath file message to $syncRoom',
+        domain: 'MATRIX_SERVICE',
+        subDomain: 'sendMatrixMsg',
+      );
+
+    try {
+      final eventId = await syncRoom?.sendFileEvent(
+        MatrixFile(
+          bytes: await File(fullPath).readAsBytes(),
+          name: fullPath,
+        ),
+        extraContent: {
+          'relativePath': relativePath,
+        },
+      );
+
+      incrementSentCount();
+
+      loggingDb.captureEvent(
+        'sent $relativePath file message to $syncRoom, event ID $eventId',
+        domain: 'MATRIX_SERVICE',
+        subDomain: 'sendMatrixMsg',
+      );
+    } catch (e, stackTrace) {
+      debugPrint('MATRIX: Error sending file: $e');
+      loggingDb.captureException(
+        e,
+        domain: 'MATRIX_SERVICE',
+        subDomain: 'sendFile',
+        stackTrace: stackTrace,
+      );
+    }
+  }
 }
