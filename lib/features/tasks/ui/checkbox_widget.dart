553772cc9804f85cd309d59c5c5834a6cdbe2351
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-06-08 02:31:53 +0200

    feat: add checklist items list

--- lib/features/tasks/ui/checkbox_widget.dart
+++ lib/features/tasks/ui/checkbox_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotti/features/tasks/state/checklist_item_controller.dart';
 
 // ignore: avoid_positional_boolean_parameters
 typedef BoolCallback = void Function(bool);
@@ -32,31 +34,76 @@ class _CheckboxItemWidgetState extends State<CheckboxItemWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 5,
-        horizontal: 10,
-      ),
-      child: CheckboxListTile(
-        title: Text(widget.title),
-        value: _isChecked,
-        controlAffinity: ListTileControlAffinity.leading,
-        secondary: widget.onEdit != null
-            ? IconButton(
-                icon: const Icon(
-                  Icons.edit,
-                  size: 20,
-                ),
-                onPressed: widget.onEdit,
-              )
-            : null,
-        onChanged: (bool? value) {
-          setState(() {
-            _isChecked = value ?? false;
-          });
-          widget.onChanged(_isChecked);
-        },
-      ),
+    return CheckboxListTile(
+      title: Text(widget.title),
+      value: _isChecked,
+      controlAffinity: ListTileControlAffinity.leading,
+      secondary: widget.onEdit != null
+          ? IconButton(
+              icon: const Icon(
+                Icons.edit,
+                size: 20,
+              ),
+              onPressed: widget.onEdit,
+            )
+          : null,
+      onChanged: (bool? value) {
+        setState(() {
+          _isChecked = value ?? false;
+        });
+        widget.onChanged(_isChecked);
+      },
+    );
+  }
+}
+
+class CheckboxItemWrapper extends ConsumerWidget {
+  const CheckboxItemWrapper(
+    this.itemId, {
+    super.key,
+  });
+
+  final String itemId;
+
+  @override
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    final provider = checklistItemControllerProvider(id: itemId);
+    final item = ref.watch(provider);
+
+    return item.map(
+      data: (data) {
+        final item = data.value;
+        if (item == null) {
+          return const SizedBox();
+        }
+        return CheckboxItemWidget(
+          title: item.data.title,
+          isChecked: item.data.isChecked,
+          onChanged: (checked) => ref.read(provider.notifier).toggleChecked(),
+        );
+      },
+      error: ErrorWidget.new,
+      loading: (_) => const CircularProgressIndicator(),
+    );
+  }
+}
+
+class CheckboxItemsList extends StatelessWidget {
+  const CheckboxItemsList({
+    required this.itemIds,
+    super.key,
+  });
+
+  final List<String> itemIds;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: itemIds.map(CheckboxItemWrapper.new).toList(),
     );
   }
 }
