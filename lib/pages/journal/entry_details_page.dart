3ae06ff31842c6c568a569df339dd08fc30c01ed
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-06-26 17:43:59 +0200

    feat: use ConsumerWidget instead of StreamBuilder

--- lib/pages/journal/entry_details_page.dart
+++ lib/pages/journal/entry_details_page.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotti/classes/journal_entities.dart';
-import 'package:lotti/database/database.dart';
-import 'package:lotti/get_it.dart';
+import 'package:lotti/features/journal/state/entry_controller.dart';
 import 'package:lotti/pages/empty_scaffold.dart';
 import 'package:lotti/utils/platform.dart';
 import 'package:lotti/widgets/app_bar/task_app_bar.dart';
@@ -14,7 +14,7 @@ import 'package:lotti/widgets/journal/entry_detail_linked.dart';
 import 'package:lotti/widgets/journal/entry_detail_linked_from.dart';
 import 'package:lotti/widgets/journal/entry_details_widget.dart';
 
-class EntryDetailPage extends StatelessWidget {
+class EntryDetailPage extends ConsumerWidget {
   const EntryDetailPage({
     required this.itemId,
     super.key,
@@ -25,55 +25,49 @@ class EntryDetailPage extends StatelessWidget {
   final bool readOnly;
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<JournalEntity?>(
-      stream: getIt<JournalDb>().watchEntityById(itemId),
-      builder: (
-        BuildContext context,
-        AsyncSnapshot<JournalEntity?> snapshot,
-      ) {
-        final item = snapshot.data;
-        if (item == null) {
-          return const EmptyScaffoldWithTitle('');
-        }
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = entryControllerProvider(id: itemId);
+    final item = ref.watch(provider).value?.entry;
 
-        return Scaffold(
-          appBar: item is Task
-              ? TaskAppBar(itemId: item.meta.id)
-              : const TitleAppBar(title: '') as PreferredSizeWidget,
-          floatingActionButton: RadialAddActionButtons(
-            linked: item,
-            radius: isMobile ? 180 : 120,
-            isMacOS: Platform.isMacOS,
-            isIOS: Platform.isIOS,
-            isAndroid: Platform.isAndroid,
-          ).animate().fadeIn(duration: const Duration(milliseconds: 500)),
-          body: SingleChildScrollView(
-            padding: const EdgeInsets.only(
-              top: 8,
-              bottom: 200,
-              left: 5,
-              right: 5,
+    if (item == null) {
+      return const EmptyScaffoldWithTitle('');
+    }
+
+    return Scaffold(
+      appBar: item is Task
+          ? TaskAppBar(itemId: item.meta.id)
+          : const TitleAppBar(title: '') as PreferredSizeWidget,
+      floatingActionButton: RadialAddActionButtons(
+        linked: item,
+        radius: isMobile ? 180 : 120,
+        isMacOS: Platform.isMacOS,
+        isIOS: Platform.isIOS,
+        isAndroid: Platform.isAndroid,
+      ).animate().fadeIn(duration: const Duration(milliseconds: 500)),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(
+          top: 8,
+          bottom: 200,
+          left: 5,
+          right: 5,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            EntryDetailWidget(
+              itemId: itemId,
+              popOnDelete: true,
+              showTaskDetails: true,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                EntryDetailWidget(
-                  itemId: itemId,
-                  popOnDelete: true,
-                  showTaskDetails: true,
-                ),
-                LinkedEntriesWidget(item: item),
-                LinkedFromEntriesWidget(item: item),
-              ],
-            ).animate().fadeIn(
-                  duration: const Duration(
-                    milliseconds: 100,
-                  ),
-                ),
-          ),
-        );
-      },
+            LinkedEntriesWidget(item: item),
+            LinkedFromEntriesWidget(item: item),
+          ],
+        ).animate().fadeIn(
+              duration: const Duration(
+                milliseconds: 100,
+              ),
+            ),
+      ),
     );
   }
 }
