23340be75f86c3c6ea08cbcb49cd042c7c6cc9a9
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-07-15 13:03:23 +0200

    feat: count time per category in about page

---
+++ lib/features/tasks/state/time_by_category_controller.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'time_by_category_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$timeByCategoryControllerHash() =>
+    r'd4d75df5880a06c02cc3fa1dc0714726894201b2';
+
+/// See also [TimeByCategoryController].
+@ProviderFor(TimeByCategoryController)
+final timeByCategoryControllerProvider = AutoDisposeAsyncNotifierProvider<
+    TimeByCategoryController, Map<String, Duration>>.internal(
+  TimeByCategoryController.new,
+  name: r'timeByCategoryControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$timeByCategoryControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TimeByCategoryController
+    = AutoDisposeAsyncNotifier<Map<String, Duration>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
