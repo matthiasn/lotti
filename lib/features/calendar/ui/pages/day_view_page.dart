063bf9b201083cecacf14f3b0f7a9e253b47915b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-08-08 23:59:10 +0200

    feat: add time by category chart modal

--- lib/features/calendar/ui/pages/day_view_page.dart
+++ lib/features/calendar/ui/pages/day_view_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:glass_kit/glass_kit.dart';
 import 'package:lotti/classes/journal_entities.dart';
-import 'package:lotti/features/calendar/ui/time_by_category_chart.dart';
 import 'package:lotti/features/calendar/ui/time_by_category_chart_card.dart';
 import 'package:lotti/features/tasks/state/day_view_controller.dart';
 import 'package:lotti/services/nav_service.dart';
@@ -96,103 +95,94 @@ class _DayViewWidgetState extends ConsumerState<DayViewWidget> {
       key: const Key('DayViewPage'),
       onVisibilityChanged:
           ref.read(dayViewControllerProvider.notifier).onVisibilityChanged,
-      child: Column(
+      child: Stack(
         children: [
-          Expanded(
-            flex: 2,
-            child: Stack(
-              children: [
-                DayView(
-                  backgroundColor: Colors.transparent,
-                  key: Key(selectedDay.ymd),
-                  showHalfHours: true,
-                  heightPerMinute: _heightPerMinute,
-                  keepScrollOffset: true,
-                  initialDay: selectedDay,
-                  headerStyle: HeaderStyle(
-                    headerTextStyle: chartTitleStyleMonospace.copyWith(
-                      fontWeight: FontWeight.w400,
-                    ),
-                    leftIcon: const Icon(
-                      Icons.arrow_back,
-                      size: 24,
-                    ),
-                    leftIconPadding: const EdgeInsets.only(
-                      left: 30,
-                      top: 10,
-                      bottom: 10,
-                    ),
-                    rightIconPadding: const EdgeInsets.only(
-                      right: 30,
-                      top: 10,
-                      bottom: 10,
-                    ),
-                    rightIcon: const Icon(
-                      Icons.arrow_forward,
-                      size: 24,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primaryContainer,
-                    ),
-                  ),
-                  dateStringBuilder: (date, {DateTime? secondaryDate}) =>
-                      date.ymwd,
-                  timeLineBuilder: timeLineBuilder,
-                  hourIndicatorSettings: HourIndicatorSettings(
-                    color: Theme.of(context).dividerColor,
-                  ),
-                  halfHourIndicatorSettings: HourIndicatorSettings(
-                    color: Theme.of(context).dividerColor,
-                    lineStyle: LineStyle.dashed,
-                  ),
-                  onEventTap: (events, date) {
-                    final event = events.firstOrNull?.event as JournalEntity?;
-                    final id = event?.id;
-                    if (id != null) {
-                      beamToNamed('/journal/$id');
-                    }
-                  },
-                  verticalLineOffset: 0,
-                  timeLineWidth: 65,
-                  liveTimeIndicatorSettings: LiveTimeIndicatorSettings(
-                    color: Colors.redAccent,
-                    showTime: true,
-                    showTimeBackgroundView: true,
-                    timeStringBuilder: (date, {DateTime? secondaryDate}) =>
-                        '  ${hhMmFormat.format(date)}',
-                  ),
-                ),
-                const Positioned(
-                  bottom: 50,
-                  right: 40,
-                  child: TimeByCategoryChartCard(),
-                ),
-                Positioned(
-                  bottom: 10,
-                  right: 40,
-                  child: GlassContainer.clearGlass(
-                    width: 200,
-                    height: 32,
-                    elevation: 0,
-                    color: Theme.of(context).shadowColor.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(15),
-                    child: Slider(
-                      inactiveColor: Theme.of(context).dividerColor,
-                      min: 0.25,
-                      max: 5,
-                      value: _heightPerMinute,
-                      onChanged: (double value) {
-                        setState(() {
-                          _heightPerMinute = value;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ],
+          DayView(
+            backgroundColor: Colors.transparent,
+            key: Key(selectedDay.ymd),
+            showHalfHours: true,
+            heightPerMinute: _heightPerMinute,
+            keepScrollOffset: true,
+            initialDay: selectedDay,
+            headerStyle: HeaderStyle(
+              headerTextStyle: chartTitleStyleMonospace.copyWith(
+                fontWeight: FontWeight.w400,
+              ),
+              leftIcon: const Icon(
+                Icons.arrow_back,
+                size: 24,
+              ),
+              leftIconPadding: const EdgeInsets.only(
+                left: 30,
+                top: 10,
+                bottom: 10,
+              ),
+              rightIconPadding: const EdgeInsets.only(
+                right: 30,
+                top: 10,
+                bottom: 10,
+              ),
+              rightIcon: const Icon(
+                Icons.arrow_forward,
+                size: 24,
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+            ),
+            dateStringBuilder: (date, {DateTime? secondaryDate}) => date.ymwd,
+            timeLineBuilder: timeLineBuilder,
+            hourIndicatorSettings: HourIndicatorSettings(
+              color: Theme.of(context).dividerColor,
+            ),
+            halfHourIndicatorSettings: HourIndicatorSettings(
+              color: Theme.of(context).dividerColor,
+              lineStyle: LineStyle.dashed,
+            ),
+            onEventTap: (events, date) {
+              final event = events.firstOrNull?.event as JournalEntity?;
+              final id = event?.id;
+              if (id != null) {
+                beamToNamed('/journal/$id');
+              }
+            },
+            verticalLineOffset: 0,
+            timeLineWidth: 65,
+            liveTimeIndicatorSettings: LiveTimeIndicatorSettings(
+              color: Colors.redAccent,
+              showTime: true,
+              showTimeBackgroundView: true,
+              timeStringBuilder: (date, {DateTime? secondaryDate}) =>
+                  '  ${hhMmFormat.format(date)}',
             ),
           ),
-          const CollapsibleTimeChart(),
+          const Positioned(
+            bottom: 50,
+            right: 40,
+            child: TimeByCategoryChartCard(),
+          ),
+          Positioned(
+            bottom: 10,
+            right: 40,
+            child: GlassContainer.clearGlass(
+              width: 200,
+              height: 32,
+              elevation: 0,
+              color: Theme.of(context).shadowColor.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(15),
+              child: Slider(
+                inactiveColor: Theme.of(context).dividerColor,
+                min: 0.25,
+                max: 5,
+                value: _heightPerMinute,
+                onChanged: (double value) {
+                  setState(() {
+                    _heightPerMinute = value;
+                  });
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/features/calendar/ui/time_by_category_chart_legend.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotti/themes/theme.dart';
+import 'package:lotti/utils/date_utils_extension.dart';
+import 'package:lotti/widgets/charts/utils.dart';
+import 'package:lotti/widgets/settings/categories/categories_type_card.dart';
+
+class TimeByCategoryChartLegend extends ConsumerWidget {
+  const TimeByCategoryChartLegend({
+    required this.selectedData,
+    super.key,
+  });
+
+  final Map<int, Map<String, dynamic>> selectedData;
+
+  @override
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    const minHeight = 150.0;
+    if (selectedData.isEmpty) {
+      return const SizedBox(height: minHeight);
+    }
+
+    final date = selectedData.values.first['date'] as DateTime;
+
+    final nonEmptyValues = selectedData.values.where((e) => e['value'] != 0);
+    var totalMinutes = 0;
+
+    for (final e in nonEmptyValues) {
+      totalMinutes += e['value'] as int;
+    }
+
+    return Container(
+      constraints: const BoxConstraints(
+        minHeight: minHeight,
+        maxWidth: 320,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                date.ymwd,
+                style: chartTitleStyleMonospace.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                formatHhMm(Duration(minutes: totalMinutes)),
+                style: chartTitleStyleMonospace.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          ...nonEmptyValues.map((e) {
+            final name = e['name'] as String;
+            final formattedValue = e['formattedValue'] as String;
+            final categoryId = e['categoryId'] as String;
+
+            return Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                children: [
+                  CategoryColorIcon(categoryId),
+                  const SizedBox(width: 10),
+                  Text(
+                    name,
+                    style: chartTitleStyleMonospace,
+                  ),
+                  const Spacer(),
+                  Text(
+                    formattedValue,
+                    style: chartTitleStyleMonospace,
+                  ),
+                ],
+              ),
+            );
+          }),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
