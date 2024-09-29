01ec4159265681fc6f281f4f68fc5166235508f0
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-09-29 21:37:32 +0200

    feat: select Whisper model

--- lib/features/speech/state/speech_settings_state.dart
+++ lib/features/speech/state/speech_settings_state.dart
@@ -3,19 +3,20 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'speech_settings_state.freezed.dart';
 
 Set<String> availableModels = {
-  'large-v3',
-  'large-v2_949MB',
-  'distil-large-v3_594MB',
-  'distil-large-v3_turbo_600MB',
+  'tiny',
+  'tiny.en',
+  'base',
+  'base.en',
   'small',
+  'small.en',
+  'large-v2',
+  'large-v3',
 };
 
 @freezed
 class SpeechSettingsState with _$SpeechSettingsState {
   factory SpeechSettingsState({
     required Set<String> availableModels,
-    required Map<String, double> downloadProgress,
-    required Map<String, double> downloadedModelSizes,
     String? selectedModel,
   }) = _SpeechSettingsState;
 }
