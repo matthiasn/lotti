342b125ed7e4026ec74be8b3d85e1485c2721cdd
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-10-23 23:24:27 +0200

    feat: surveys in wolt modal sheet

--- lib/widgets/misc/wolt_modal_config.dart
+++ lib/widgets/misc/wolt_modal_config.dart
@@ -1,7 +1,7 @@
 class WoltModalConfig {
   static const pageBreakpoint = 560;
   static const pagePadding = 16.0;
-  static const maxDialogWidth = 560.0;
+  static const maxDialogWidth = 600.0;
   static const minDialogWidth = 400.0;
   static const minPageHeight = 0.3;
   static const maxPageHeight = 0.9;
