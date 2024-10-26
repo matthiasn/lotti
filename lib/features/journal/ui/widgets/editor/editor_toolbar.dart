3b67d36550f748e17a9f14ab593315c29ae90ce7
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-10-26 22:17:16 +0200

    feat: update editor content when entry updated outside editor

--- lib/features/journal/ui/widgets/editor/editor_toolbar.dart
+++ lib/features/journal/ui/widgets/editor/editor_toolbar.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotti/features/journal/state/entry_controller.dart';
 
-class ToolbarWidget extends StatelessWidget {
+class ToolbarWidget extends ConsumerWidget {
   const ToolbarWidget({
     required this.controller,
+    required this.entryId,
     super.key,
   });
 
   final QuillController controller;
+  final String entryId;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = entryControllerProvider(id: entryId);
+    final notifier = ref.read(provider.notifier);
     const duration = Duration(milliseconds: 400);
     const curve = Curves.easeInOutQuint;
     const height = 60.0;
 
-    return Material(
+    final toolbar = Material(
       elevation: 1,
       child: QuillToolbar.simple(
         controller: controller,
@@ -37,24 +43,33 @@ class ToolbarWidget extends StatelessWidget {
           showDividers: false,
         ),
       ),
-    )
-        .animate()
-        .scaleY(
-          duration: duration,
-          curve: curve,
-          begin: 0,
-          end: 1,
-          alignment: Alignment.topCenter,
-        )
-        .custom(
-          duration: duration,
-          curve: curve,
-          builder: (context, value, child) {
-            return SizedBox(
-              height: height * value,
-              child: child, // child is the Text widget being animated
-            );
-          },
-        );
+    );
+
+    if (notifier.animationCompleted) {
+      return SizedBox(
+        height: height,
+        child: toolbar,
+      );
+    } else {
+      return toolbar
+          .animate(onComplete: (_) => notifier.animationCompleted = true)
+          .scaleY(
+            duration: duration,
+            curve: curve,
+            begin: 0,
+            end: 1,
+            alignment: Alignment.topCenter,
+          )
+          .custom(
+            duration: duration,
+            curve: curve,
+            builder: (context, value, child) {
+              return SizedBox(
+                height: height * value,
+                child: child, // child is the Text widget being animated
+              );
+            },
+          );
+    }
   }
 }
