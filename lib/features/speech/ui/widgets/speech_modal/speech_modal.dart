d5ba0b495c232a05db8db5992b0f446c166a94e1
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-11-17 19:15:27 +0100

    feat: transcribe button in speech modal

--- lib/features/speech/ui/widgets/speech_modal/speech_modal.dart
+++ lib/features/speech/ui/widgets/speech_modal/speech_modal.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotti/classes/journal_entities.dart';
 import 'package:lotti/features/journal/state/entry_controller.dart';
+import 'package:lotti/features/speech/ui/widgets/speech_modal/transcribe_button.dart';
 import 'package:lotti/features/speech/ui/widgets/speech_modal/transcripts_list.dart';
 import 'package:lotti/l10n/app_localizations_context.dart';
 import 'package:lotti/themes/theme.dart';
@@ -32,6 +33,8 @@ class SpeechModal {
               ),
               child: Column(
                 children: [
+                  const SizedBox(height: 20),
+                  TranscribeButton(entryId: entryId),
                   TranscriptsList(entryId: entryId),
                 ],
               ),
---
+++ lib/features/speech/ui/widgets/speech_modal/transcribe_button.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotti/classes/journal_entities.dart';
+import 'package:lotti/features/journal/state/entry_controller.dart';
+import 'package:lotti/features/speech/state/asr_service.dart';
+import 'package:lotti/features/speech/ui/widgets/transcription_progress_modal.dart';
+import 'package:lotti/get_it.dart';
+import 'package:lotti/l10n/app_localizations_context.dart';
+
+class TranscribeButton extends ConsumerWidget {
+  const TranscribeButton({
+    required this.entryId,
+    super.key,
+  });
+
+  final String entryId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (!(Platform.isMacOS || Platform.isIOS)) {
+      return const SizedBox.shrink();
+    }
+
+    final provider = entryControllerProvider(id: entryId);
+
+    final notifier = ref.read(provider.notifier);
+
+    final entryState = ref.watch(provider).value;
+
+    final item = entryState?.entry;
+    if (item == null || item is! JournalAudio) {
+      return const SizedBox.shrink();
+    }
+
+    return TextButton(
+      onPressed: () async {
+        final isQueueEmpty = getIt<AsrService>().enqueue(entry: item);
+
+        if (await isQueueEmpty) {
+          if (!context.mounted) return;
+          await TranscriptionProgressModal.show(context);
+        }
+
+        await Future<void>.delayed(
+          const Duration(milliseconds: 100),
+        );
+        notifier
+          ..setController()
+          ..emitState();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(Icons.transcribe_rounded),
+            const SizedBox(width: 10),
+            Text(context.messages.speechModalAddTranscription),
+          ],
+        ),
+      ),
+    );
+  }
+}
