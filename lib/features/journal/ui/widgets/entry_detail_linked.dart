fa86759e4c32f78d936b527834c07b5765fde17e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2024-12-30 00:25:27 +0100

    feat: use new linked entries controller

--- lib/features/journal/ui/widgets/entry_detail_linked.dart
+++ lib/features/journal/ui/widgets/entry_detail_linked.dart
@@ -1,153 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotti/classes/journal_entities.dart';
-import 'package:lotti/database/database.dart';
+import 'package:lotti/features/journal/state/linked_entries_controller.dart';
 import 'package:lotti/features/journal/ui/widgets/entry_details_widget.dart';
-import 'package:lotti/get_it.dart';
 import 'package:lotti/l10n/app_localizations_context.dart';
 import 'package:lotti/themes/theme.dart';
-import 'package:lotti/utils/consts.dart';
 import 'package:lotti/widgets/modal/modal_action_sheet.dart';
 import 'package:lotti/widgets/modal/modal_sheet_action.dart';
 
-class LinkedEntriesWidget extends StatefulWidget {
-  const LinkedEntriesWidget({
-    required this.item,
+class LinkedEntriesWidget extends ConsumerWidget {
+  const LinkedEntriesWidget(
+    this.item, {
     super.key,
   });
 
   final JournalEntity item;
 
   @override
-  State<LinkedEntriesWidget> createState() => _LinkedEntriesWidgetState();
-}
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    final provider = linkedEntriesControllerProvider(id: item.id);
+    final notifier = ref.read(provider.notifier);
+    final entryLinks = ref.watch(provider).valueOrNull ?? [];
 
-class _LinkedEntriesWidgetState extends State<LinkedEntriesWidget> {
-  bool _includeHidden = false;
-  bool _releaseHideLinkedEntries = false;
+    final includeHiddenProvider = includeHiddenControllerProvider(id: item.id);
+    final includeHiddenNotifier = ref.read(includeHiddenProvider.notifier);
+    final includeHidden = ref.watch(includeHiddenProvider);
 
-  @override
-  void initState() {
-    super.initState();
-    getIt<JournalDb>().getConfigFlag(releaseHideLinkedEntries).then((value) {
-      setState(() {
-        _releaseHideLinkedEntries = value;
-      });
-    });
-  }
+    final itemIds = entryLinks.map((link) => link.toId).toList();
 
-  @override
-  Widget build(BuildContext context) {
-    final db = getIt<JournalDb>();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              context.messages.journalLinkedEntriesLabel,
+              style: TextStyle(
+                color: context.colorScheme.outline,
+              ),
+            ),
+            const SizedBox(width: 40),
+            Text(
+              // TODO: l10n
+              'hidden:',
+              style: TextStyle(color: context.colorScheme.outline),
+            ),
+            Checkbox(
+              value: includeHidden,
+              onChanged: (value) {
+                includeHiddenNotifier.includeHidden = value ?? false;
+              },
+            ),
+          ],
+        ),
+        ...List.generate(
+          itemIds.length,
+          (int index) {
+            final itemId = itemIds.elementAt(index);
 
-    return StreamBuilder<List<String>>(
-      stream: db.watchLinkedEntityIds(
-        widget.item.meta.id,
-        includeHidden: _includeHidden,
-      ),
-      builder: (context, itemsSnapshot) {
-        if (itemsSnapshot.data == null || itemsSnapshot.data!.isEmpty) {
-          return Container();
-        } else {
-          final itemIds = itemsSnapshot.data!;
-
-          return Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    context.messages.journalLinkedEntriesLabel,
-                    style: TextStyle(
-                      color: context.colorScheme.outline,
-                    ),
+            // TODO: move to extended entry actions
+            Future<void> unlink() async {
+              const unlinkKey = 'unlinkKey';
+              final result = await showModalActionSheet<String>(
+                context: context,
+                title: context.messages.journalUnlinkQuestion,
+                actions: [
+                  ModalSheetAction(
+                    icon: Icons.warning,
+                    label: context.messages.journalUnlinkConfirm,
+                    key: unlinkKey,
+                    isDestructiveAction: true,
+                    isDefaultAction: true,
                   ),
-                  if (_releaseHideLinkedEntries) ...[
-                    const SizedBox(width: 40),
-                    Text(
-                      // TODO: l10n
-                      'hidden:',
-                      style: TextStyle(
-                        color: context.colorScheme.outline,
-                      ),
-                    ),
-                    // TODO: move to filter bottom sheet, use controller
-                    Checkbox(
-                      value: _includeHidden,
-                      onChanged: (value) {
-                        setState(() {
-                          _includeHidden = value ?? false;
-                        });
-                      },
-                    ),
-                  ],
                 ],
-              ),
-              ...List.generate(
-                itemIds.length,
-                (int index) {
-                  final itemId = itemIds.elementAt(index);
+              );
 
-                  Future<void> unlink() async {
-                    const unlinkKey = 'unlinkKey';
-                    final result = await showModalActionSheet<String>(
-                      context: context,
-                      title: context.messages.journalUnlinkQuestion,
-                      actions: [
-                        ModalSheetAction(
-                          icon: Icons.warning,
-                          label: context.messages.journalUnlinkConfirm,
-                          key: unlinkKey,
-                          isDestructiveAction: true,
-                          isDefaultAction: true,
-                        ),
-                      ],
-                    );
+              if (result == unlinkKey) {
+                await notifier.removeLink(toId: itemId);
+              }
+            }
 
-                    if (result == unlinkKey) {
-                      await db.removeLink(
-                        fromId: widget.item.meta.id,
-                        toId: itemId,
-                      );
-                    }
-                  }
-
-                  Future<void> toggleHiddenLink() async {
-                    const hideLinkKey = 'hideLinkKey';
-                    final result = await showModalActionSheet<String>(
-                      context: context,
-                      title: context.messages.journalUnlinkQuestion,
-                      actions: [
-                        ModalSheetAction(
-                          icon: Icons.warning,
-                          label: context.messages.journalUnlinkConfirm,
-                          key: hideLinkKey,
-                          isDefaultAction: true,
-                        ),
-                      ],
-                    );
-
-                    if (result == hideLinkKey) {
-                      await db.removeLink(
-                        fromId: widget.item.meta.id,
-                        toId: itemId,
-                      );
-                    }
-                  }
-
-                  return EntryDetailWidget(
-                    key: Key('$itemId-$itemId'),
-                    itemId: itemId,
-                    popOnDelete: false,
-                    unlinkFn: unlink,
-                    parentTags: widget.item.meta.tagIds?.toSet(),
-                    linkedFrom: widget.item,
-                  );
-                },
-              ),
-            ],
-          );
-        }
-      },
+            return EntryDetailWidget(
+              key: Key('$itemId-$itemId'),
+              itemId: itemId,
+              popOnDelete: false,
+              unlinkFn: unlink,
+              parentTags: item.meta.tagIds?.toSet(),
+              linkedFrom: item,
+            );
+          },
+        ),
+      ],
     );
   }
 }
