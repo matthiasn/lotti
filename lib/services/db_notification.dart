e07d794810f2bc511c6cb2f05de488ae4539f2cf
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-01-04 23:44:59 +0100

    feat: shorten sync UI update delay

--- lib/services/db_notification.dart
+++ lib/services/db_notification.dart
@@ -14,7 +14,7 @@ class UpdateNotifications {
   void notify(Set<String> affectedIds, {bool fromSync = false}) {
     if (fromSync) {
       _affectedIdsFromSync.addAll(affectedIds);
-      _fromSyncTimer ??= Timer(const Duration(seconds: 5), () {
+      _fromSyncTimer ??= Timer(const Duration(seconds: 2), () {
         if (_affectedIdsFromSync.isNotEmpty) {
           _controller.add({..._affectedIdsFromSync});
           _affectedIdsFromSync.clear();
