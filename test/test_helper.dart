fff7a7d777637d4fccd909f78f29b9b5ebbc12a4
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-02-02 01:44:19 +0100

    test: add tests for category create modal

--- test/test_helper.dart
+++ test/test_helper.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Widget createTestApp(Widget child) {
-  return ProviderScope(
-    child: MaterialApp(
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: Scaffold(
-        body: child,
+  return MaterialApp(
+    localizationsDelegates: const [
+      AppLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: AppLocalizations.supportedLocales,
+    home: Scaffold(
+      body: ConstrainedBox(
+        constraints: const BoxConstraints(minHeight: 800),
+        child: child,
       ),
     ),
   );
