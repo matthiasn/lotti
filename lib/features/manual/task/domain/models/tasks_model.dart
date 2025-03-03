7c1ae5fcba6abd98083d8e1be951c32428f0cd8c
cyberk1ng <acepython001@gmail.com>
2025-03-03 23:23:14 +0100

    fix: hide manual text in wolt modal sheet

--- lib/features/manual/task/domain/models/tasks_model.dart
+++ lib/features/manual/task/domain/models/tasks_model.dart
@@ -2,15 +2,13 @@ class TaskManual {
   final String title;
   final String steps;
   final String? imagePath;
-  final double imageWidth;
   final bool? imageFirst;
 
   TaskManual({
     required this.title,
     required this.steps,
     this.imagePath,
-    this.imageWidth = 110,
-    this.imageFirst,
 
+    this.imageFirst,
   });
 }
--- lib/features/manual/task/presentation/widgets/manual_content_row.dart
+++ lib/features/manual/task/presentation/widgets/manual_content_row.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:lotti/features/manual/task/domain/models/tasks_model.dart';
+import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 
 class ManualContentRow extends StatelessWidget {
   const ManualContentRow({required this.content, super.key});
   final TaskManual content;
+
+  void _showModalSheet(BuildContext context) {
+    WoltModalSheet.show<void>(
+      context: context,
+      modalTypeBuilder: (context) => WoltModalType.dialog(),
+      pageListBuilder: (context) => [
+        WoltModalSheetPage(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              content.steps,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          isTopBarLayerAlwaysVisible: true,
+          trailingNavBarWidget: IconButton(
+            onPressed: Navigator.of(context).pop,
+            icon: const Icon(Icons.close),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final imageWidget = Container(
@@ -11,28 +36,21 @@ class ManualContentRow extends StatelessWidget {
         border: Border.all(color: Colors.red),
         borderRadius: BorderRadius.circular(10),
       ),
-      width: content.imageWidth,
+      width: 200,
       child: Image.asset(content.imagePath!),
     );
 
-    final textWidget = SizedBox(
-      width: 120,
-      child: Text(
-        content.steps,
-        style: const TextStyle(
-          fontSize: 13,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+    final questionIcon = IconButton(
+      icon: const Icon(Icons.help_outline, color: Colors.blue, size: 24),
+      onPressed: () => _showModalSheet(context),
     );
-
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: content.imageFirst!
-            ? [imageWidget, textWidget]
-            : [textWidget, imageWidget],
+            ? [imageWidget, questionIcon]
+            : [questionIcon, imageWidget],
       ),
     );
   }
