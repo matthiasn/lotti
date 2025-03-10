576fe61afa71ccf465247c77581752a1f6dbd1d0
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-03-10 23:13:18 +0100

    feat: swipe left to remove

--- lib/features/tasks/ui/checklists/checklist_suggestions_widget.dart
+++ lib/features/tasks/ui/checklists/checklist_suggestions_widget.dart
@@ -6,7 +6,7 @@ import 'package:lotti/l10n/app_localizations_context.dart';
 import 'package:lotti/themes/theme.dart';
 import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 
-class ChecklistSuggestionsWidget extends ConsumerWidget {
+class ChecklistSuggestionsWidget extends ConsumerStatefulWidget {
   const ChecklistSuggestionsWidget({
     required this.itemId,
     super.key,
@@ -15,15 +15,20 @@ class ChecklistSuggestionsWidget extends ConsumerWidget {
   final String itemId;
 
   @override
-  Widget build(
-    BuildContext context,
-    WidgetRef ref,
-  ) {
-    final checklistItems = ref
-        .watch(
-          checklistItemSuggestionsControllerProvider(id: itemId),
-        )
-        .valueOrNull;
+  ConsumerState<ChecklistSuggestionsWidget> createState() =>
+      _ChecklistSuggestionsWidgetState();
+}
+
+class _ChecklistSuggestionsWidgetState
+    extends ConsumerState<ChecklistSuggestionsWidget> {
+  final removedItems = <String>{};
+
+  @override
+  Widget build(BuildContext context) {
+    final provider =
+        checklistItemSuggestionsControllerProvider(id: widget.itemId);
+    final notifier = ref.read(provider.notifier);
+    final checklistItems = ref.watch(provider).valueOrNull;
 
     if (checklistItems == null || checklistItems.isEmpty) {
       return const SizedBox.shrink();
@@ -38,32 +43,87 @@ class ChecklistSuggestionsWidget extends ConsumerWidget {
             color: context.colorScheme.outline,
           ),
         ),
-        const SizedBox(height: 10),
-        ...checklistItems.map(
-          (checklistItem) => Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: DragItemWidget(
-              dragItemProvider: (request) async {
-                final dragItem = DragItem(
-                  localData: {
-                    'checklistItemTitle': checklistItem.title,
-                  },
-                )..add(Formats.plainText(checklistItem.title));
-                return dragItem;
-              },
-              allowedOperations: () => [DropOperation.move],
-              child: DraggableWidget(
-                child: ChecklistItemWidget(
-                  title: checklistItem.title,
-                  isChecked: checklistItem.isChecked,
-                  onChanged: (checked) {},
-                  showEditIcon: false,
-                  readOnly: true,
+        ...checklistItems
+            .where((item) => !removedItems.contains(item.title))
+            .map(
+              (checklistItem) => DragItemWidget(
+                dragItemProvider: (request) async {
+                  final dragItem = DragItem(
+                    localData: {
+                      'checklistItemTitle': checklistItem.title,
+                    },
+                  )..add(Formats.plainText(checklistItem.title));
+                  return dragItem;
+                },
+                allowedOperations: () => [DropOperation.move],
+                child: DraggableWidget(
+                  child: Dismissible(
+                    key: Key(checklistItem.title),
+                    dismissThresholds: const {
+                      DismissDirection.endToStart: 0.25
+                    },
+                    onDismissed: (_) {
+                      notifier.removeActionItem(title: checklistItem.title);
+                      setState(() {
+                        removedItems.add(checklistItem.title);
+                      });
+                    },
+                    background: Container(
+                      color: context.colorScheme.error,
+                      height: double.infinity,
+                      child: const Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Icon(Icons.delete, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    direction: DismissDirection.endToStart,
+                    confirmDismiss: (_) async {
+                      final result = await showDialog<bool>(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(context.messages.checklistItemDelete),
+                            content: Text(
+                              context.messages.checklistItemDeleteWarning,
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: Text(
+                                  context.messages.checklistItemDeleteCancel,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(true),
+                                child: Text(
+                                  context.messages.checklistItemDeleteConfirm,
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                      return result ?? false;
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: ChecklistItemWidget(
+                        title: checklistItem.title,
+                        isChecked: checklistItem.isChecked,
+                        onChanged: (checked) {},
+                        showEditIcon: false,
+                        readOnly: true,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
       ],
     );
   }
