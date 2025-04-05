e6665d65a574ff40294a140f331ecf6067674fb3
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-04-06 01:06:46 +0200

    feat: move task form out of entry card

--- lib/features/tasks/ui/task_form.dart
+++ lib/features/tasks/ui/task_form.dart
@@ -1,46 +1,41 @@
-// ignore_for_file: avoid_dynamic_calls
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotti/classes/journal_entities.dart';
 import 'package:lotti/features/ai/state/consts.dart';
 import 'package:lotti/features/ai/ui/latest_ai_response_summary.dart';
 import 'package:lotti/features/journal/state/entry_controller.dart';
-import 'package:lotti/features/journal/ui/widgets/editor/editor_widget.dart';
 import 'package:lotti/features/tasks/ui/checklists/checklists_widget.dart';
 
 class TaskForm extends ConsumerWidget {
-  const TaskForm(
-    this.task, {
+  const TaskForm({
+    required this.taskId,
     super.key,
   });
 
-  final Task task;
+  final String taskId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final entryId = task.meta.id;
-    final provider = entryControllerProvider(id: entryId);
+    final provider = entryControllerProvider(id: taskId);
     final entryState = ref.watch(provider).value;
+    final task = entryState?.entry;
 
-    if (entryState == null) {
+    if (task == null || task is! Task) {
       return const SizedBox.shrink();
     }
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        EditorWidget(entryId: entryId),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
-          child: LatestAiResponseSummary(
-            id: entryId,
-            aiResponseType: taskSummary,
-          ),
-        ),
+        // EditorWidget(entryId: taskId),
         const SizedBox(height: 10),
-        ChecklistsWidget(entryId: entryId, task: task),
+        LatestAiResponseSummary(
+          id: taskId,
+          aiResponseType: taskSummary,
+        ),
         const SizedBox(height: 20),
+        ChecklistsWidget(entryId: taskId, task: task),
+        const SizedBox(height: 10),
       ],
     );
   }
