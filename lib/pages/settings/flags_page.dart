2312f8939363360dc610af51d82c798757673ad5
cyberk1ng <acepython001@gmail.com>
2025-04-09 14:18:12 +0200

    feat: config flag page showcase implementation

--- lib/pages/settings/flags_page.dart
+++ lib/pages/settings/flags_page.dart
@@ -6,9 +6,31 @@ import 'package:lotti/l10n/app_localizations_context.dart';
 import 'package:lotti/pages/settings/sliver_box_adapter_page.dart';
 import 'package:lotti/utils/consts.dart';
 import 'package:lotti/widgets/settings/config_flag_card.dart';
+import 'package:showcaseview/showcaseview.dart';
 
-class FlagsPage extends StatelessWidget {
+class FlagsPage extends StatefulWidget {
   const FlagsPage({super.key});
+
+  @override
+  State<FlagsPage> createState() => _FlagsPageState();
+}
+
+class _FlagsPageState extends State<FlagsPage> {
+  final _privateFlagKey = GlobalKey();
+  final _embeddingFlagKey = GlobalKey();
+  final _autoTranscribeFlagKey = GlobalKey();
+  final _enableMatrixFlagKey = GlobalKey();
+  final _enableTooltipFlagKey = GlobalKey();
+  final _recordLocationFlagKey = GlobalKey();
+  final _resendAttachmentsFlagKey = GlobalKey();
+  final _enableLoggingFlagKey = GlobalKey();
+  final _useCloudInferenceFlagKey = GlobalKey();
+  final _enableNotificationFlagKey = GlobalKey();
+  final _enableAutoTaskTldrFlagKey = GlobalKey();
+
+  final _enableHabitsPageFlagKey = GlobalKey();
+  final _enableDashboardsPageFlagKey = GlobalKey();
+  final _enableCalendarPageFlagKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -38,23 +60,85 @@ class FlagsPage extends StatelessWidget {
         };
 
         final filteredItems =
-            items.where((flag) => displayedItems.contains(flag.name));
+            items.where((flag) => displayedItems.contains(flag.name)).toList();
 
-        return SliverBoxAdapterPage(
-          title: context.messages.settingsFlagsTitle,
-          showBackButton: true,
-          child: Column(
-            children: [
-              ...filteredItems.mapIndexed(
-                (index, flag) => ConfigFlagCard(
-                  item: flag,
-                  index: index,
-                ),
+        return ShowCaseWidget(
+          builder: (context) => SliverBoxAdapterShowcasePage(
+            showcaseIcon: IconButton(
+              onPressed: () {
+                ShowCaseWidget.of(context).startShowCase([
+                  _privateFlagKey,
+                  _embeddingFlagKey,
+                  _autoTranscribeFlagKey,
+                  _enableMatrixFlagKey,
+                  _enableTooltipFlagKey,
+                  _recordLocationFlagKey,
+                  _resendAttachmentsFlagKey,
+                  _enableLoggingFlagKey,
+                  _useCloudInferenceFlagKey,
+                  _enableNotificationFlagKey,
+                  _enableAutoTaskTldrFlagKey,
+                  _enableHabitsPageFlagKey,
+                  _enableDashboardsPageFlagKey,
+                  _enableCalendarPageFlagKey,
+                ]);
+              },
+              icon: const Icon(
+                Icons.info_outline_rounded,
               ),
-            ],
+            ),
+            title: context.messages.settingsFlagsTitle,
+            showBackButton: true,
+            child: Column(
+              children: [
+                ...filteredItems.mapIndexed(
+                  (index, flag) => ConfigFlagCard(
+                    item: flag,
+                    index: index,
+                    showcaseKey: _getShowcaseKeyForFlag(flag.name),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
     );
+  }
+
+  GlobalKey? _getShowcaseKeyForFlag(String flagName) {
+    switch (flagName) {
+      case privateFlag:
+        return _privateFlagKey;
+      case attemptEmbedding:
+        return _embeddingFlagKey;
+      case autoTranscribeFlag:
+        return _autoTranscribeFlagKey;
+      case enableMatrixFlag:
+        return _enableMatrixFlagKey;
+      case enableTooltipFlag:
+        return _enableTooltipFlagKey;
+      case recordLocationFlag:
+        return _recordLocationFlagKey;
+      case resendAttachments:
+        return _resendAttachmentsFlagKey;
+      case enableLoggingFlag:
+        return _enableLoggingFlagKey;
+      case useCloudInferenceFlag:
+        return _useCloudInferenceFlagKey;
+      case enableNotificationsFlag:
+        return _enableNotificationFlagKey;
+      case enableAutoTaskTldrFlag:
+        return _enableAutoTaskTldrFlagKey;
+      case enableHabitsPageFlag:
+        return _enableHabitsPageFlagKey;
+      case enableDashboardsPageFlag:
+        return _enableDashboardsPageFlagKey;
+      case enableCalendarPageFlag:
+        return _enableCalendarPageFlagKey;
+
+      default:
+        return null;
+    }
   }
 }
--- test/pages/settings/flags_page_test.dart
+++ test/pages/settings/flags_page_test.dart
@@ -1,48 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:lotti/database/database.dart';
 import 'package:lotti/features/user_activity/state/user_activity_service.dart';
-import 'package:lotti/get_it.dart';
 import 'package:lotti/pages/settings/flags_page.dart';
+import 'package:lotti/pages/settings/sliver_box_adapter_page.dart';
+import 'package:lotti/services/db_notification.dart';
+import 'package:lotti/utils/consts.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:showcaseview/showcaseview.dart';
 
 import '../../mocks/mocks.dart';
 import '../../widget_test_utils.dart';
 
+class MockJournalDb extends Mock implements JournalDb {}
+
+class MockUserActivityService extends Mock implements UserActivityService {}
+
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  late MockJournalDb mockDb;
+  late MockUserActivityService mockUserActivityService;
+  final mockUpdateNotifications = MockUpdateNotifications();
 
-  final mockJournalDb = MockJournalDb();
+  setUp(() async {
+    mockDb = MockJournalDb();
+    mockUserActivityService = MockUserActivityService();
+
+    when(() => mockUpdateNotifications.updateStream).thenAnswer(
+      (_) => Stream<Set<String>>.fromIterable([]),
+    );
+
+    when(() => mockDb.watchConfigFlags()).thenAnswer(
+      (_) => Stream<Set<ConfigFlag>>.fromIterable([
+        {
+          const ConfigFlag(
+            name: privateFlag,
+            description: 'Show private entries?',
+            status: true,
+          ),
+        },
+      ]),
+    );
+
+    when(() => mockUserActivityService.updateActivity()).thenReturn(null);
+
+    GetIt.I
+      ..registerSingleton<JournalDb>(mockDb)
+      ..registerSingleton<UserActivityService>(mockUserActivityService)
+      ..registerSingleton<UpdateNotifications>(mockUpdateNotifications);
+  });
+
+  tearDown(() async {
+    await GetIt.I.reset();
+  });
 
   group('FlagsPage Widget Tests - ', () {
-    setUp(() {
-      when(mockJournalDb.watchConfigFlags).thenAnswer(
-        (_) => Stream<Set<ConfigFlag>>.fromIterable([
-          <ConfigFlag>{
-            const ConfigFlag(
-              name: 'private',
-              description: 'Show private entries?',
-              status: true,
-            ),
-          }
-        ]),
-      );
-
-      getIt
-        ..registerSingleton<JournalDb>(mockJournalDb)
-        ..registerSingleton<UserActivityService>(UserActivityService());
-    });
-    tearDown(getIt.reset);
-
     testWidgets('page is displayed', (tester) async {
       await tester.pumpWidget(
         makeTestableWidget(
-          ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxHeight: 1000,
-              maxWidth: 1000,
+          ShowCaseWidget(
+            builder: (context) => ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 1000,
+                maxWidth: 1000,
+              ),
+              child: const FlagsPage(),
             ),
-            child: const FlagsPage(),
           ),
         ),
       );
@@ -51,5 +77,46 @@ void main() {
 
       expect(find.text('Show private entries?'), findsOneWidget);
     });
+  });
+
+  testWidgets('FlagsPage with showcase displays correctly', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const MediaQuery(
+          data: MediaQueryData(size: Size(800, 600)),
+          child: SliverBoxAdapterShowcasePage(
+            title: 'Flags',
+            showcaseIcon: Icon(Icons.info),
+            child: FlagsPage(),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    // Verify the showcase AppBar is displayed with the info icon in the title
+    final appBar = find.byType(AppBar).first;
+    expect(appBar, findsOneWidget);
+    expect(find.byIcon(Icons.info), findsOneWidget);
+
+    // Verify the private flag is displayed
+    expect(find.text('Show private entries?'), findsOneWidget);
+
+    // Verify the back button is present in the AppBar
+    final backButton = find.descendant(
+      of: appBar,
+      matching: find.byIcon(Icons.chevron_left),
+    );
+    expect(backButton, findsOneWidget);
   });
 }
