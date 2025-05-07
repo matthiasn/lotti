f0ee32d8d5cf270c56c740e7f65c014b17f2fb09
cyberk1ng <acepython001@gmail.com>
2025-05-07 22:16:15 +0200

    Update Android configuration and Gradle settings

---
+++ android/app/src/main/kotlin/com/example/lotti/FlutterHealthFitPlugin.kt
@@ -0,0 +1,32 @@
+package com.example.lotti
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+class FlutterHealthFitPlugin: FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "flutter_health_fit")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        when (call.method) {
+            "getPlatformVersion" -> {
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+} 
---
+++ android/app/src/main/kotlin/com/example/lotti/FlutterNativeTimezonePlugin.kt
@@ -0,0 +1,32 @@
+package com.example.lotti
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import java.util.TimeZone
+
+class FlutterNativeTimezonePlugin: FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "flutter_native_timezone")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        when (call.method) {
+            "getLocalTimezone" -> {
+                result.success(TimeZone.getDefault().id)
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+} 
