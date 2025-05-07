939a5e19e5bd706471d835769ad09d0ab3d9740c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-05-08 00:32:09 +0200

    chore: replace discontinued flutter_markdown lib

--- lib/features/speech/ui/widgets/transcription_progress_modal.dart
+++ lib/features/speech/ui/widgets/transcription_progress_modal.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:gpt_markdown/gpt_markdown.dart';
 import 'package:lotti/features/ai/ui/animation/ai_running_animation.dart';
 import 'package:lotti/features/speech/state/asr_service.dart';
 import 'package:lotti/get_it.dart';
@@ -42,12 +42,12 @@ class TranscriptionProgressModalContent extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              MarkdownBody(
-                data: text,
-                styleSheet: MarkdownStyleSheet(
-                  p: TextStyle(
-                    color: hasError ? context.colorScheme.error : null,
-                  ),
+              SelectionArea(
+                child: GptMarkdown(
+                  text,
+                  style: hasError
+                      ? TextStyle(color: context.colorScheme.error)
+                      : null,
                 ),
               ),
               verticalModalSpacer,
