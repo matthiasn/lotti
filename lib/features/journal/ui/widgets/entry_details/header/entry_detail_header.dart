08d16d7b2fd42585bb3ba07bb5b7af8d10bd4886
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-05-26 23:11:37 +0200

    refactor: remove old hardcoded AI inference code

--- lib/features/journal/ui/widgets/entry_details/header/entry_detail_header.dart
+++ lib/features/journal/ui/widgets/entry_details/header/entry_detail_header.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotti/classes/entry_link.dart';
 import 'package:lotti/classes/journal_entities.dart';
-import 'package:lotti/features/ai/ui/ai_popup_menu.dart';
 import 'package:lotti/features/ai/ui/unified_ai_popup_menu.dart';
 import 'package:lotti/features/categories/ui/widgets/category_selection_icon_button.dart';
 import 'package:lotti/features/journal/state/entry_controller.dart';
@@ -86,16 +85,11 @@ class _EntryDetailHeaderState extends ConsumerState<EntryDetailHeader> {
                 ),
               if (entry != null && entry is Task ||
                   entry is JournalImage ||
-                  entry is JournalAudio) ...[
-                AiPopUpMenu(
+                  entry is JournalAudio)
+                UnifiedAiPopUpMenu(
                   journalEntity: entry!,
                   linkedFromId: widget.linkedFromId,
                 ),
-                UnifiedAiPopUpMenu(
-                  journalEntity: entry,
-                  linkedFromId: widget.linkedFromId,
-                ),
-              ],
               IconButton(
                 icon: Icon(
                   Icons.more_horiz,
