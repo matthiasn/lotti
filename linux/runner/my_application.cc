061588047ac865f77da178076e454e3123605e02
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2025-07-19 19:52:56 +0200

    feat: adjust window title

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "lotti");
+    gtk_header_bar_set_title(header_bar, "Lotti");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "lotti");
+    gtk_window_set_title(window, "Lotti");
   }
 
   gtk_window_set_icon_from_file(window, "assets/icon/app_icon_1024.png", NULL);
