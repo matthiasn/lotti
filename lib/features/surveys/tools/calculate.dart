013f0a0431b2fb2e8efed5d4330d91fd547817dc
cberk1ng <acepython001@gmail.com>
2025-11-17 11:46:20 +0100

    fix: revert calculate.dart to use switch expression

--- lib/features/surveys/tools/calculate.dart
+++ lib/features/surveys/tools/calculate.dart
@@ -18,11 +18,12 @@ Map<String, int> calculateScores({
       final stepResult = results[questionId] as RPStepResult?;
       final choice = stepResult?.results['answer'];
 
-      final value = choice is RPImageChoice
-          ? choice.value as int
-          : choice is List<RPChoice>
-              ? choice.firstOrNull?.value ?? 0
-              : 0;
+      final value = switch (choice) {
+        (final RPImageChoice c) => c.value as int,
+        (final List<RPChoice> c) => c.firstOrNull?.value ?? 0,
+        Object() => 0,
+        null => 0,
+      };
 
       score = score + value;
     }
