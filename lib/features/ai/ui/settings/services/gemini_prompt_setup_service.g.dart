be44f144cd2b92b29290ecae497264c2e45f394c
George Johnson <150244095+cyberk1ng@users.noreply.github.com>
2025-12-05 00:53:15 +0100

    feat: auto-create default prompts when adding Gemini provider (#2487)

---
+++ lib/features/ai/ui/settings/services/gemini_prompt_setup_service.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gemini_prompt_setup_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geminiPromptSetupServiceHash() =>
+    r'2dbc176b45c20f89661c53ba101480fa2a56a37c';
+
+/// Provider for [GeminiPromptSetupService].
+///
+/// Copied from [geminiPromptSetupService].
+@ProviderFor(geminiPromptSetupService)
+final geminiPromptSetupServiceProvider =
+    AutoDisposeProvider<GeminiPromptSetupService>.internal(
+  geminiPromptSetupService,
+  name: r'geminiPromptSetupServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$geminiPromptSetupServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GeminiPromptSetupServiceRef
+    = AutoDisposeProviderRef<GeminiPromptSetupService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
