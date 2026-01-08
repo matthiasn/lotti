7a3c6576682ef23db999e72cddc917e00fb2d929
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2026-01-08 10:40:01 +0100

    chore: remove unused manifest_tool (#2579)

--- flatpak/manifest_tool/build_utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Build utilities for Flatpak Flutter applications."""
-
-from .utils import find_flutter_sdk
-
-__all__ = [
-    "find_flutter_sdk",
-]
--- flatpak/manifest_tool/prepare/__init__.py
+++
@@ -1,9 +0,0 @@
-"""High-level orchestration for preparing Flathub submissions."""
-
-from .orchestrator import PrepareFlathubOptions, PrepareFlathubError, prepare_flathub
-
-__all__ = [
-    "PrepareFlathubOptions",
-    "PrepareFlathubError",
-    "prepare_flathub",
-]
