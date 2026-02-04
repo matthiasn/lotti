f9c809faa5ebc4fc05244ec050916862ceb21d40
cberk1ng <acepython001@gmail.com>
2026-02-04 09:14:08 +0100

    fix: add non-null assertion for coverArtId in GameyTaskCard

--- lib/widgets/gamey/gamey_task_card.dart
+++ lib/widgets/gamey/gamey_task_card.dart
@@ -75,7 +75,7 @@ class GameyTaskCard extends StatelessWidget {
               bottom: _bottomPadding,
             ),
       child: hasCoverArt
-          ? _buildWithCoverArt(context, coverArtId)
+          ? _buildWithCoverArt(context, coverArtId!)
           : _buildStandardContent(context),
     );
   }
