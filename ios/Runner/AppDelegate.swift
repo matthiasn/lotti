4574c39dc7c48a714fe843ac03038da6362a65e3
cberk1ng <acepython001@gmail.com>
2026-02-18 11:02:31 +0100

    fix: safely unwrap optional registrar in iOS AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,7 +13,9 @@ import Flutter
         
         let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
 
-        AudioConverter.register(with: self.registrar(forPlugin: "AudioConverter")!)
+        if let audioConverterRegistrar = self.registrar(forPlugin: "AudioConverter") {
+            AudioConverter.register(with: audioConverterRegistrar)
+        }
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
