567e0e096ba4b6c62e8034f8505a497c72ddf5f2
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2026-08-05 21:46:46 +0200

    test: make integration tag cleanup idempotent

--- integration_test/home_integration_test.dart
+++ integration_test/home_integration_test.dart
@@ -56,16 +56,25 @@ void main() {
           expect(find.text('Flags'), findsOneWidget);
           expect(find.text('Maintenance'), findsOneWidget);
 
+          const testTag = 'integration-test-tag';
           await tester.tap(find.byIcon(MdiIcons.tag));
           await tester.pumpAndSettle();
+
+          // Make local retries idempotent if an earlier run stopped before
+          // reaching the cleanup at the end of this test.
+          if (find.text(testTag).evaluate().isNotEmpty) {
+            await tester.tap(find.text(testTag));
+            await tester.pumpAndSettle();
+            await tester.tap(find.byIcon(MdiIcons.trashCanOutline));
+            await tester.pumpAndSettle();
+            expect(find.text(testTag), findsNothing);
+          }
 
           await tester.tap(find.byKey(const Key('add_tag_action')));
           await tester.pumpAndSettle();
 
           await tester.tap(find.byIcon(MdiIcons.tagPlusOutline));
           await tester.pumpAndSettle();
-
-          const testTag = 'integration-test-tag';
 
           await tester.enterText(
             find.byKey(const Key('tag_name_field')),
